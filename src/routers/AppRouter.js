ba3c0ba23cd3e46550d252322eb9419d0fb7a139
amitlavi7 <amitlavi7@gmail.com>
2020-04-06 17:11:49 +0300

    Setup production webpack build

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import ExpensDashboardPage from '../components/ExpensDashboardPge';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
@@ -12,7 +12,7 @@ const AppRouter = () => (
     <div>
         <Header/>
         <Switch>
-            <Route path="/" component={ExpensDashboardPage} exact={true} />
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit/:id" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
